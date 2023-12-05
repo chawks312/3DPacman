55eb3f9439551c42af19fb181b077688523bdb70
chawks312 <eric.liu.2025@gmail.com>
2023-12-05 01:26:34 -0800

    added ghost prefab & tried to do collision

--- 3D Pacman!/Assets/Scripts/Movement.cs
+++ 3D Pacman!/Assets/Scripts/Movement.cs
@@ -7,7 +7,8 @@ public class Movement : MonoBehaviour
     public float speed = 5.0f;
     public float sensitivity = 2.0f;
     public float jump_power = 5.0f;
-
+    public GameObject loseTextObject;  // text displayed when a player wins
+    public GameObject againButton;        // button to play again
     public int jump_counter = 10;
 
     private Rigidbody rb;
@@ -15,10 +16,12 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        print("start");
        // Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
         rb = GetComponent<Rigidbody>();
+        Reset();
     }
 
     // Update is called once per frame
@@ -40,8 +43,29 @@ public class Movement : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter(Collision other)
+    {
+        print("woah collision!" + other.gameObject.tag);
+        if (other.gameObject.CompareTag("Ghost"))
+        {
+            print("Collision with Ghost");
+            loseTextObject.SetActive(true);
+            againButton.SetActive(true);
+        }
+    }
+
     void Jump() {
         rb.velocity = new Vector3(0, jump_power, 0);
         jump_counter--;
+    }
+
+    public void Reset()
+    {
+        print("Reset");
+        // rb.transform.position = new Vector3(0f, 2f, 0f);
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        loseTextObject.SetActive(false);
+        againButton.SetActive(false);
     }
 }
